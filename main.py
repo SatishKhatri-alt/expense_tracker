92a6773f893dd8d8bce99d9236180a42230cf19f
SatishDev92 <kcsatish404@gmail.com>
2026-01-04 09:05:37 +0545

    tigger

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import random
 
 mcp = FastMCP(name="DemoMCP")
 
-@mcp.tool
-def roll_dice(n_dice: int) -> list[int]:
-    """Roll a specified number of six-sided dice."""
-    return [random.randint(1, 7) for _ in range(n_dice)]
-
-@mcp.tool
-def add_two_numbers(a: float,b:float)->float:
-    """Add two numbers and return the result."""
-    return a + b
 
 if __name__ == "__main__":
     mcp.run()
